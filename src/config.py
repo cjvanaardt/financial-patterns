7820fbe4332b55d914c0fb84aafcfede7d91c23c
Christiaan van Aardt <christiaanjvanaardt@gmail.com>
2025-04-01 18:23:53 +1100

    started work on scraping historical finance data from yahoo finance

---
+++ src/config.py
@@ -0,0 +1,10 @@
+""""used to hold all constants"""
+
+# default date historic data is captured from
+DEFAULT_DATE = date(1900, 01, 01)
+
+# Yahoo Finance uses this value to represent a day in their history range
+YAHOO_DAY_VALUE = 86400
+
+# Yahoo Finance has set period=0 as this date
+YAHOO_DEFAULT = date(1970, 01, 01)
---
+++ src/medallions/iron.py
@@ -0,0 +1,10 @@
+"""" module docstring """"
+
+def extract_historical_data(code, begin=):
+    """function docstring:
+    input: 
+    """
+    print("Hello")
+
+
+a = [10, 14, 13, "j"]
