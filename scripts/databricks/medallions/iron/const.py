d3aa5a190c9d1d03cf887bba9055dc41057fd9fa
cjvanaardt <cjvanaardt>
2025-04-14 07:43:17 +0000

    Adding databricks specific scripts to pull data from Tiingo to azure data lake storage

---
+++ scripts/databricks/medallions/iron/const.py
@@ -0,0 +1,22 @@
+# tickers for which new data will be pulled each day
+TICKERS = ["voo"]
+
+# tickers for which we will pull historical data
+HIST_TICKERS = []
+
+# financial-patterns location
+MODULE_PATH = "/Workspace/Repos/christiaanvanaardt@hotmail.com/financial-patterns/"
+
+# date which we will pull historical data from
+HIST_DATE_YEAR = 2000
+HIST_DATE_MONTH = 1
+HIST_DATE_DAY = 1
+
+# storage file we will use
+STORAGE_FILE = "parquet"
+
+# storage account
+STORAGE_ACCOUNT = "stfinancialpatterns01"
+
+# bronze container
+CONTAINER = "bronze"
---
+++ scripts/databricks/medallions/iron/tiingo_daily.py
@@ -0,0 +1,22 @@
+import sys, os, const
+import datetime as dt
+
+# import from financial-patterns module
+sys.path.append(os.path.abspath(const.MODULE_PATH))
+
+import src.medallions.iron.iron as i
+import sensitive
+
+# get today's date
+today = datetime.now()
+
+# save data 
+for ticker in const.TICKERS:
+    i.save_tiingo_to_adls(
+    ticker,
+    i.API_TOKEN,
+    const.STORAGE_FILE, 
+    const.STORAGE_ACCOUNT, 
+    sensitive.STORAGE_ACCOUNT_KEY, 
+    const.CONTAINER, 
+    f"/Tiingo_EOD/{ticker}/{today.year}/{today.month}/{today.day}/")
